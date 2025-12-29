16facaf463e0b6c1dcd0d6d9b3234c61bbab63ef
harsh <harshyadav8932@gmail.com>
2025-12-30 03:08:45 +0530

    changes to About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,11 @@ import about from '../assets/about.svg';
 import SectionTitle from './SectionTitle';
 const About = () => {
   return (
-    <section className="bg-white py-20" id="about">
-      <div className="align-element grid md: grid-cols-2 items-center gap-60">
+    <section className="bg-white py-20 " id="about">
+      <div className="align-element grid md:grid-cols-2 items-center gap-16">
         <img src={about} className="w-full h-64" />
         <article>
-          <SectionTitle text="about me" className="" />
+          <SectionTitle text="about me" />
           <p className="text-slate-600 mt-8 leading-loose">
             I’m a software developer with 3+ years of experience in building applications with
             React, Java, and Spring Boot. I’ve also worked with C/C++ to design
